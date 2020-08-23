e09b41dc1e42f668a90856b9a8f294b8229f1f0b
Eric Thomas <ewolft@gmail.com>
2020-08-23 12:32:06 -0400

    More descriptive name for index var

--- memory.cpp
+++ memory.cpp
@@ -18,19 +18,19 @@ void Memory::write(uint16_t addr, uint8_t val) {
 	data[addr] = val;
 	if (addr < 0x2000) { // Zero Page mirroring
 		for (int i = 1; i <= 3; ++i) {
-			uint16_t temp = addr + 0x800 * i;
-			if (temp >= 0x2000) {
-				temp -= 0x2000;
+			uint16_t index = addr + 0x800 * i;
+			if (index >= 0x2000) {
+				index -= 0x2000;
 			}
-			data[temp] = val;
+			data[index] = val;
 		}
 	} else if (addr < 0x4000) { // I/O register mirroring
 		for (int i = 1; i <= 400; ++i) {
-			uint16_t temp = addr + 0x8 * i;
-			if (temp >= 0x4000) {
-				temp -= 0x2000;
+			uint16_t index = addr + 0x8 * i;
+			if (index >= 0x4000) {
+				index -= 0x2000;
 			}
-			data[temp] = val;
+			data[index] = val;
 		}
 	}
 }
