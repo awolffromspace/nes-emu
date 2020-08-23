1b7fa044c4604afae2607d80428025b01aac39c8
Eric Thomas <ewolft@gmail.com>
2020-08-23 13:32:22 -0400

    Add ROR

--- cpu.cpp
+++ cpu.cpp
@@ -771,7 +771,34 @@ void CPU::ROL() {
 }
 
 void CPU::ROR() {
-
+	if (op.status & Op::Reread) {
+		mem.read(op.tempAddr);
+	} else if (op.status & Op::WriteUnmodified) {
+		mem.write(op.tempAddr, op.val);
+		uint8_t temp = op.val >> 1;
+		if (P & 1) {
+			temp |= 0x80;
+		}
+		if (op.val & 1) {
+			P |= 1;
+		} else {
+			P &= 0xfe;
+		}
+		op.val = temp;
+		if (op.val == 0) {
+			P |= 2;
+		} else {
+			P &= 0xfd;
+		}
+		if (op.val & 0x80) {
+			P |= 0x80;
+		} else {
+			P &= 0x7f;
+		}
+	} else if (op.status & Op::WriteModified) {
+		mem.write(op.tempAddr, op.val);
+		op.status |= Op::Done;
+	}
 }
 
 void CPU::RRA() {
